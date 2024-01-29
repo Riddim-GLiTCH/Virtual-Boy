ce661e8543916710092f3cb759400e7a2380f7c6
Skye <Riddim_GLiTCH@courvix.com>
2024-01-29 13:20:32 +0100

    Update bd-scss.config.js
    Bump version 1.5
    VIRTUAL BOY LIVES!!!

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -2,15 +2,15 @@
 export default {
     meta: {
         name: "Virtual Boy",
-        author: "Riddim_Girl",
-        version: "1.4",
+        author: "Riddim_Glitch",
+        version: "1.5",
         description: "A theme based on a failed Nintendo console with the same name.",
-        source: "https://github.com/Riddim-GLiTCH/Virtual-Boy",
+        source: "https://github.com/Nexus-Themes/Virtual-Boy",
         invite: "aYxpgkvdvR",
         authorId: "801089753038061669"
     },
     fileName: "virtual-boy",
-    baseImport: "https://Riddim-GLiTCH.github.io/Virtual-Boy/virtual-boy.css",
+    baseImport: "https://Nexus-Themes.github.io/Virtual-Boy/virtual-boy.css",
     addons: [
         ["src/addon/_messageboxes.scss", "dist/MessageBoxes.css"],
         ["src/addon/_textglow.scss", "dist/TextGlow.css"],
